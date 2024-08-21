b71de8bef8616cac46c12824f59d7fe98fce609a
Rahila074 <rahilamuhammed074@gmail.com>
2024-08-21 20:25:06 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+# what is matplotlib ?
+#matplotlib is a low level gragh plotting library in python
+#that serves as a visualizastion utility
+
+
+#checking matplolib version?
+
+
+
+import matplotlib
+
+print(matplotlib.__version__)
